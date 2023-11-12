93b81a963a1faf957ccebe72be10e37e5dab005d
x287xtesting <147965172+x287xtesting@users.noreply.github.com>
2023-11-12 23:27:19 +0300

    Rename result in index.js and change resultMessage in index.html

--- js/index.js
+++ js/index.js
@@ -4,12 +4,17 @@ const choices = {
   scissors: ["scissors", "✂️"],
 }
 
-const messages = {
+const resultMessage = {
   win: "You Win...",
   lose: "Round is Lost :(",
   tie: "It's a tie",
 }
 
+const finalResult = {
+  win: "You Won the Game!",
+  lose: "You Lost the Game :(",
+  tie: "It's a Draw!"
+}
 const betweenChoices = {
   win: "beats",
   lose: "loses to",

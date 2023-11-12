5021b2079ab10516c52c71df2e90b256115e5f5e
x287xtesting <147965172+x287xtesting@users.noreply.github.com>
2023-11-12 23:18:16 +0300

    Add constant messages and move playRound to the Game class

---
+++ js/Game.js
@@ -0,0 +1,37 @@
+class Game {
+  constructor() {
+    console.log("New Game Has Started");
+    this.playerScore = 0;
+    this.computerScore = 0;
+    this.finished = false;
+  }
+
+  incrementPlayerScore() {
+    this.playerScore++;
+  }
+  incrementComputerScore() {
+    this.computerScore++;
+  }
+
+  /**
+   * 
+   * @param {string} computerChoice - lowercase random rock paper or scissors
+   * @param {string} playerChoice - lowercase emoji button id
+   * @returns {number} 1 for winning / 0 for draw / -1 for losing
+   */
+  static playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+      return 0;
+    }
+    else if (
+      (playerChoice == "rock" && computerChoice == "scissors") ||
+      (playerChoice == "scissors" && computerChoice == "paper") ||
+      (playerChoice == "paper" && computerChoice == "rock")
+    ) {
+      return 1;
+    } else {
+      return - 1;
+    }
+  }
+}
+
--- js/index.js
+++ js/index.js
@@ -4,44 +4,20 @@ const choices = {
   scissors: ["scissors", "✂️"],
 }
 
+const messages = {
+  win: "You Win...",
+  lose: "Round is Lost :(",
+  tie: "It's a tie",
+}
+
+const betweenChoices = {
+  win: "beats",
+  lose: "loses to",
+  tie: "nutralizes",
+}
+
 function getComputerChoice() {
   const rpc = Object.getOwnPropertyNames(choices);
   return rpc[Math.floor(Math.random() * rpc.length)];
-}
-
-
-// I'll have the playRound() function return an object with all the 
-// results I need with the score and everything
-
-/**
- * @param {string} playerSelection
- * @param {string} computerSelection
- * @returns {string}
- */
-function playRound(playerSelection, computerSelection) {
-
-  playerSelection = playerSelection.toLowerCase();
-  computerSelection = computerSelection.toLowerCase();
-
-  if (playerSelection === computerSelection) {
-    return "Draw!";
-  }
-  switch (playerSelection) {
-    case "rock":
-      if (computerSelection === "scissors")
-        return "You Win! Rock beats Scissors";
-      return "You Lose! Rock loses to Paper";
-    case "scissors":
-      if (computerSelection === "paper")
-        return "You Win! Scissors beat Paper";
-      return "You Lose! Scissors lose to Rock";
-    case "paper":
-      if (computerSelection === "rock")
-        return "You Win! Paper beats Rock"
-      return "You Lose! Paper loses to Scissors";
-    default:
-      return "Choose either Rock, Paper, or Scissors";
-  }
-
 }
 

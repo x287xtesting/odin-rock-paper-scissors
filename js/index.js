e69eb7e261744b58607200cf2b1013992f8ce372
x287xtesting <147965172+x287xtesting@users.noreply.github.com>
2023-11-12 23:34:53 +0300

    Add DOM variables

--- js/index.js
+++ js/index.js
@@ -4,13 +4,13 @@ const choices = {
   scissors: ["scissors", "✂️"],
 }
 
-const resultMessage = {
+const resultMessages = {
   win: "You Win...",
   lose: "Round is Lost :(",
   tie: "It's a tie",
 }
 
-const finalResult = {
+const finalResults = {
   win: "You Won the Game!",
   lose: "You Lost the Game :(",
   tie: "It's a Draw!"
@@ -25,6 +25,23 @@ const initalScores = {
   computer: "❔",
   player: "❓",
 }
+
+const resultMessage = document.querySelector("#resultMesssage");
+const playerSelection = document.querySelector("#playerSelection");
+const computerSelection = document.querySelector("#computerSelection");
+const betweenSelections = document.querySelector("#betweenSelections");
+const playerEmoji = document.querySelector("#playerEmoji");
+const computerEmoji = document.querySelector("#computerEmoji");
+const playerScore = document.querySelector("#playerScore");
+const computerScore = document.querySelector("#computerScore");
+const buttons = document.querySelector("#buttons");
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+const playBtn = document.querySelector("#playBtn");
+const countdownElement = document.querySelector("#countdownSpan");
+const finalResult = document.querySelector("#finalResult");
+const newGameBtn = document.querySelector("#newGameBtn");
 
 function getComputerChoice() {
   const rpc = Object.getOwnPropertyNames(choices);

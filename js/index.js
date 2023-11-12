e8fb4f890d3f72999db6a90f585f3d341006eaa0
x287xtesting <147965172+x287xtesting@users.noreply.github.com>
2023-11-12 22:53:22 +0300

    Add CountDown.js and update getComputerChoices() in index.js

---
+++ js/index.js
@@ -0,0 +1,47 @@
+const choices = {
+  rock: ["Rock", "🪨"],
+  paper: ["Paper", "🗞️"],
+  scissors: ["scissors", "✂️"],
+}
+
+function getComputerChoice() {
+  const rpc = Object.getOwnPropertyNames(choices);
+  return rpc[Math.floor(Math.random() * rpc.length)];
+}
+
+
+// I'll have the playRound() function return an object with all the 
+// results I need with the score and everything
+
+/**
+ * @param {string} playerSelection
+ * @param {string} computerSelection
+ * @returns {string}
+ */
+function playRound(playerSelection, computerSelection) {
+
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+
+  if (playerSelection === computerSelection) {
+    return "Draw!";
+  }
+  switch (playerSelection) {
+    case "rock":
+      if (computerSelection === "scissors")
+        return "You Win! Rock beats Scissors";
+      return "You Lose! Rock loses to Paper";
+    case "scissors":
+      if (computerSelection === "paper")
+        return "You Win! Scissors beat Paper";
+      return "You Lose! Scissors lose to Rock";
+    case "paper":
+      if (computerSelection === "rock")
+        return "You Win! Paper beats Rock"
+      return "You Lose! Paper loses to Scissors";
+    default:
+      return "Choose either Rock, Paper, or Scissors";
+  }
+
+}
+
